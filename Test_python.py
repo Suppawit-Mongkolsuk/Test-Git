a677f43346d1a72aa50f6fd74147e12fad8c59c2
Suppawit-Mongkolsuk <157823980+Suppawit-Mongkolsuk@users.noreply.github.com>
2024-01-27 23:30:48 +0700

    Add age

--- Test_python.py
+++ Test_python.py
@@ -1,5 +1,6 @@
 Fistname = input('กรุณากรอกชื่อจริง : ')
 Lastname = input('กรุณากรอกนามสกุล : ')
+age = int(input('กรุณากรอกอายุ : '))
 
-print('ชิ่อของคถุณคือ:',Fistname + ' ' + Lastname)
+print('ชิ่อของคถุณคือ:',Fistname + ' ' + Lastname + ' คุณอายุ :' + str(age))
 
